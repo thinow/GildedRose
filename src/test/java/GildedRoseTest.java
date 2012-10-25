b079f80f4103e80b0f585a89da59c9764635d9ee
Thierry Nowak <thinow@gmail.com>
2012-10-25 13:41:41 +0200

    Harnais de test : évalue les items après exécution du main.

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,26 @@
-import static org.junit.Assert.*;
+import static org.fest.assertions.Assertions.*;
+
+import java.util.List;
 
 import org.junit.Test;
 
-
 public class GildedRoseTest {
 
+	private static final String[] NO_ARGUMENTS = null;
+
+	private static final Object[] EXPECTED_NAMES = new String[] { "+5 Dexterity Vest", "Aged Brie",
+			"Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros",
+			"Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake" };
+
 	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	public void evaluateItemsAfterRunExecutable() {
+		// when
+		GildedRose.main(NO_ARGUMENTS);
+
+		// then
+		List<Item> items = GildedRose.getItems();
+		assertThat(items).onProperty("name").containsExactly(EXPECTED_NAMES);
+		assertThat(items).onProperty("sellIn").containsExactly(9, 1, 4, 0, 14, 2);
+		assertThat(items).onProperty("quality").containsExactly(19, 1, 6, 80, 21, 5);
 	}
 }
