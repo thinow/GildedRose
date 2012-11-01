0c29cc2181a2d02d7feb08d9caeacb8e7781a05f
Thierry Nowak <thinow@gmail.com>
2012-11-01 12:55:34 +0100

    Utilisation de Lombok sur Item.

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,32 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Item {
-    public String name;
-	public int sellIn; 
-    public int quality; 
-    
-    public Item(String name, int sellIn, int quality) {
-		this.setName(name);
-		this.setSellIn(sellIn);
-		this.setQuality(quality);
-	}
-    
-	/* Generated getter and setter code */
-    public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getSellIn() {
-		return sellIn;
-	}
-	public void setSellIn(int sellIn) {
-		this.sellIn = sellIn;
-	}
-	public int getQuality() {
-		return quality;
-	}
-	public void setQuality(int quality) {
-		this.quality = quality;
-	}
+
+	private String name;
+	private int sellIn;
+	private int quality;
+
 }
