5cf87508014a88f0c0770140c5274deaa3d3f6bd
Thierry Nowak <thinow@gmail.com>
2012-11-01 23:33:28 +0100

    Liaison des items et des policies dans le store.

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -1,36 +1,34 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class GildedRose {
 
-	private static List<Item> items = null;
+	private static final StandardPolicy STANDARD_POLICY = new StandardPolicy();
+	private static final CheesePolicy CHEESE_POLICY = new CheesePolicy();
+	private static final ConcertPassPolicy PASS_POLICY = new ConcertPassPolicy();
+	private static final LegendaryPolicy LEGENDARY_POLICY = new LegendaryPolicy();
+
+	private static Store store;
 
 	public static void main(String[] args) {
 
-		items = new ArrayList<Item>();
-		items.add(new Item("+5 Dexterity Vest", 10, 20));
-		items.add(new Item("Aged Brie", 2, 0));
-		items.add(new Item("Elixir of the Mongoose", 5, 7));
-		items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-		items.add(new Item("Conjured Mana Cake", 3, 6));
+		store = new Store();
 
-		update();
+		store.add(new Item("+5 Dexterity Vest", 10, 20), STANDARD_POLICY);
+		store.add(new Item("Aged Brie", 2, 0), CHEESE_POLICY);
+		store.add(new Item("Elixir of the Mongoose", 5, 7), STANDARD_POLICY);
+		store.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80), LEGENDARY_POLICY);
+		store.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20), PASS_POLICY);
+		store.add(new Item("Conjured Mana Cake", 3, 6), STANDARD_POLICY);
+
+		store.updateItems();
 	}
 
 	public static void update() {
-		for (Item item : items) {
-			update(item);
-		}
-	}
-
-	private static void update(Item item) {
-		ItemUpdater updater = new ItemUpdater(item);
-		updater.update();
+		store.updateItems();
 	}
 
 	public static List<Item> getItems() {
-		return items;
+		return store.getItems();
 	}
 
 }
---
+++ src/main/java/Store.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+public class Store {
+
+	@AllArgsConstructor
+	private class ItemAndPolicy {
+		@Getter
+		private Item item;
+		@Getter
+		private Policy policy;
+	}
+
+	private Collection<ItemAndPolicy> itemsAndPolicies = new ArrayList<ItemAndPolicy>();
+
+	public void add(Item item, Policy policy) {
+		itemsAndPolicies.add(new ItemAndPolicy(item, policy));
+	}
+
+	public void updateItems() {
+		for (ItemAndPolicy itemAndPolicy : itemsAndPolicies) {
+			Policy policy = itemAndPolicy.getPolicy();
+			Item item = itemAndPolicy.getItem();
+
+			update(item, policy);
+		}
+	}
+
+	private void update(Item item, Policy policy) {
+		policy.updateSellIn(item);
+		policy.updateQuality(item);
+	}
+
+	public List<Item> getItems() {
+		List<Item> items = new ArrayList<Item>(itemsAndPolicies.size());
+		for (ItemAndPolicy itemAndPolicy : itemsAndPolicies) {
+			items.add(itemAndPolicy.getItem());
+		}
+		return items;
+	}
+}
