4b22c6033c72e8439ca6c52acb01f0b52316eae1
Thierry Nowak <thinow@gmail.com>
2012-11-01 13:09:40 +0100

    Utilisation de Lombok pour la délégation des getter/setter.

--- src/main/java/ItemUpdater.java
+++ src/main/java/ItemUpdater.java
@@ -1,50 +1,53 @@
 import static java.lang.Math.*;
 import lombok.AllArgsConstructor;
+import lombok.Delegate;
 
 @AllArgsConstructor
 public class ItemUpdater {
+
+	@Delegate
 	private Item item;
 
 	public void update() {
-		if ("Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+		if ("Sulfuras, Hand of Ragnaros".equals(getName())) {
 			return;
 		}
 
 		decreaseSellIn();
 
-		if ("Aged Brie".equals(item.getName())) {
+		if ("Aged Brie".equals(getName())) {
 			increaseQuality();
-			if (item.getSellIn() < 0)
+			if (getSellIn() < 0)
 				increaseQuality();
-		} else if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+		} else if ("Backstage passes to a TAFKAL80ETC concert".equals(getName())) {
 			increaseQuality();
-			if (item.getSellIn() < 10)
+			if (getSellIn() < 10)
 				increaseQuality();
-			if (item.getSellIn() < 5)
+			if (getSellIn() < 5)
 				increaseQuality();
-			if (item.getSellIn() < 0)
+			if (getSellIn() < 0)
 				removeQuality();
 		} else {
 			decreaseQuality();
-			if (item.getSellIn() < 0)
+			if (getSellIn() < 0)
 				decreaseQuality();
 		}
 	}
 
 	private void decreaseSellIn() {
-		item.setSellIn(item.getSellIn() - 1);
+		setSellIn(getSellIn() - 1);
 	}
 
 	private void increaseQuality() {
-		item.setQuality(min(item.getQuality() + 1, 50));
+		setQuality(min(getQuality() + 1, 50));
 	}
 
 	private void decreaseQuality() {
-		item.setQuality(max(item.getQuality() - 1, 0));
+		setQuality(max(getQuality() - 1, 0));
 	}
 
 	private void removeQuality() {
-		item.setQuality(0);
+		setQuality(0);
 	}
 
 }
