079cb6380f3aa08f6ba8f2c57d15c70be119419b
Thierry Nowak <thinow@gmail.com>
2012-11-01 23:36:33 +0100

    Refactoring sur les méthodes de Item.

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -6,24 +6,27 @@ import lombok.Data;
 @AllArgsConstructor
 public class Item {
 
+	private static final int MIN_QUALITY = 0;
+	private static final int MAX_QUALITY = 50;
+
 	private String name;
 	private int sellIn;
 	private int quality;
 
 	public void decreaseSellIn() {
-		setSellIn(getSellIn() - 1);
+		sellIn -= 1;
 	}
 
 	public void increaseQuality() {
-		setQuality(min(getQuality() + 1, 50));
+		quality = min(quality + 1, MAX_QUALITY);
 	}
 
 	public void decreaseQuality() {
-		setQuality(max(getQuality() - 1, 0));
+		quality = max(quality - 1, MIN_QUALITY);
 	}
 
 	public void removeQuality() {
-		setQuality(0);
+		quality = 0;
 	}
 
 }
